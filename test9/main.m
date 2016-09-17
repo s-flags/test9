0420b83c8d466e3af12e8352225029380b126117
Sander Paas <sander.paas@mobisolutions.com>
2016-09-17 15:24:40 +0300

    Initial Commit

---
+++ test9/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test9
+//
+//  Created by sander on 17.09.16.
+//  Copyright © 2016 Flagstone. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
